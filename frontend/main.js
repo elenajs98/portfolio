3f506484556457ea92ebda28d9cd9bdbc9249f96
elenajs_98 <elebrax98@gmail.com>
2022-03-12 13:53:20 +0100

    ya he conseguido lo del alert

--- frontend/main.js
+++ frontend/main.js
@@ -1,19 +1,21 @@
 window.onload = () => {
+   //Capturamos el formulario
    let form = document.getElementById("form");
-
+   //estamos a la espera de que se pulse el boton
    form.addEventListener("submit", (e) => {
+      //evitamos el comportamiento por defecto
       e.preventDefault();
-
+      //Capturamos cada imput de texto
       let name = document.getElementById("name").value;
       let email = document.getElementById("email").value;
       let text = document.getElementById("field").value;
-
+      //con esta función descargamos el archivo
       function download(filename, text) {
          var element = document.createElement('a');
          element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
          element.setAttribute('download', filename);
 
-         element.style.display = 'block';
+         element.style.display = 'none';
          document.body.appendChild(element);
 
          element.click();
@@ -21,14 +23,16 @@ window.onload = () => {
          document.body.removeChild(element);
       }
 
-
+      //construimos la variable de tecto
       let tempText = `
          Emisor: ${name}, \n
          Email: ${email},\n
          Mensaje: ${text}`;
 
+      //invocamos a la funcion descargar
+      download("email.txt", tempText);
+      alert("Formulario enviado con éxito");
 
-      download("email.txt",tempText);
-
+      
    });
 } 
